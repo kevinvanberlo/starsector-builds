69ccef6f60f60769e9bf2dcf77ba152d617583ec
Kevin van Berlo <kevinvberlo@proton.me>
2025-12-18 13:27:26 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,325 @@
+const DATA = {
+  "Capitals": {
+    "Astral": [
+      "Build/Capitals/Astral/2yiV7fG.png",
+      "Build/Capitals/Astral/4qGVp5t.png",
+      "Build/Capitals/Astral/vQ6uuHG.png"
+    ],
+    "Atlas_MkII": [
+      "Build/Capitals/Atlas_MkII/ABOty6W.png",
+      "Build/Capitals/Atlas_MkII/i4rdttt.png"
+    ],
+    "Conquest": [
+      "Build/Capitals/Conquest/9DJGt22.png",
+      "Build/Capitals/Conquest/ixS1XCe.png"
+    ],
+    "Executor": [
+      "Build/Capitals/Executor/wdnlTHM.png"
+    ],
+    "Invictus": [
+      "Build/Capitals/Invictus/dYN2TDE.png"
+    ],
+    "Legion": [
+      "Build/Capitals/Legion/MEuyLVW.jpeg",
+      "Build/Capitals/Legion/ahDVSdr.png",
+      "Build/Capitals/Legion/cfuGmMc.png",
+      "Build/Capitals/Legion/cuOpQaW.png"
+    ],
+    "Legion_XIV": [
+      "Build/Capitals/Legion_XIV/esAkUZL.png",
+      "Build/Capitals/Legion_XIV/jRhJp4P.png",
+      "Build/Capitals/Legion_XIV/pGDW7gj.jpeg"
+    ],
+    "Odyssey": [
+      "Build/Capitals/Odyssey/infctaE.png",
+      "Build/Capitals/Odyssey/uDCjaMk.png"
+    ],
+    "Onslaught": [
+      "Build/Capitals/Onslaught/4YMoh3v.png",
+      "Build/Capitals/Onslaught/WOo7Qot.png",
+      "Build/Capitals/Onslaught/dNdZgAa.png"
+    ],
+    "Paragon": [
+      "Build/Capitals/Paragon/4pR6wBg.png",
+      "Build/Capitals/Paragon/6j7TXYP.png",
+      "Build/Capitals/Paragon/Qi5XZBf.png",
+      "Build/Capitals/Paragon/xFoqofp.png"
+    ],
+    "Pegasus": [
+      "Build/Capitals/Pegasus/Q9HFlpx.png"
+    ],
+    "Prometheus_MkII": [
+      "Build/Capitals/Prometheus_MkII/WeZJ98Z.png",
+      "Build/Capitals/Prometheus_MkII/aMH70Ry.png"
+    ],
+    "Retribution": [
+      "Build/Capitals/Retribution/bTVnp5U.png"
+    ]
+  },
+  "Cruisers": {
+    "Apogee": [
+      "Build/Cruisers/Apogee/6pFz8hc.jpg",
+      "Build/Cruisers/Apogee/Tivn5Uy.png",
+      "Build/Cruisers/Apogee/WkiAf7l.png"
+    ],
+    "Aurora": [
+      "Build/Cruisers/Aurora/8gxM5pX.png",
+      "Build/Cruisers/Aurora/oC0jDTe.png"
+    ],
+    "Champion": [
+      "Build/Cruisers/Champion/6aPIjB1.png",
+      "Build/Cruisers/Champion/GlKP6fv.png",
+      "Build/Cruisers/Champion/kOu49m6.png",
+      "Build/Cruisers/Champion/p5Qr7m5.png"
+    ],
+    "Dominator": [
+      "Build/Cruisers/Dominator/cLNeBZq.png",
+      "Build/Cruisers/Dominator/tECTwHT.png",
+      "Build/Cruisers/Dominator/xdMUdK6.png"
+    ],
+    "Doom": [
+      "Build/Cruisers/Doom/OsPvrTr.png"
+    ],
+    "Eagle": [
+      "Build/Cruisers/Eagle/2IXblHK.png",
+      "Build/Cruisers/Eagle/4qkED2e.png",
+      "Build/Cruisers/Eagle/BtrmxoI.png",
+      "Build/Cruisers/Eagle/DvhqxWg.png",
+      "Build/Cruisers/Eagle/o2lJV0o.png",
+      "Build/Cruisers/Eagle/vltptLi.png"
+    ],
+    "Eradicator": [
+      "Build/Cruisers/Eradicator/4jnIjEM.png",
+      "Build/Cruisers/Eradicator/5Vv2Fnc.png",
+      "Build/Cruisers/Eradicator/HejTLac.png",
+      "Build/Cruisers/Eradicator/IwHdjVX.png",
+      "Build/Cruisers/Eradicator/U8n3kbP.png"
+    ],
+    "Falcon": [
+      "Build/Cruisers/Falcon/pTaCFS6.png"
+    ],
+    "Falcon_P": [
+      "Build/Cruisers/Falcon_P/6pieY6M.png",
+      "Build/Cruisers/Falcon_P/P4qnqpp.png",
+      "Build/Cruisers/Falcon_P/aDqXdaF.png"
+    ],
+    "Fury": [
+      "Build/Cruisers/Fury/FfQmr7P.png",
+      "Build/Cruisers/Fury/Hx8pt7r.png"
+    ],
+    "Grendel": [
+      "Build/Cruisers/Grendel/kGMoPD8.png"
+    ],
+    "Gryphon": [
+      "Build/Cruisers/Gryphon/4EQJfJH.png",
+      "Build/Cruisers/Gryphon/cnrHYPF.png",
+      "Build/Cruisers/Gryphon/iAFq8fe.png"
+    ],
+    "Heron": [
+      "Build/Cruisers/Heron/0Solsar.png",
+      "Build/Cruisers/Heron/X17zhCM.png",
+      "Build/Cruisers/Heron/eAruwR5.png"
+    ],
+    "Mora": [
+      "Build/Cruisers/Mora/uv4k0es.png"
+    ],
+    "Venture": [
+      "Build/Cruisers/Venture/U6H3muW.png",
+      "Build/Cruisers/Venture/alTxPex.png",
+      "Build/Cruisers/Venture/bJ7FvA7.png"
+    ],
+    "Venture_P": [
+      "Build/Cruisers/Venture_P/CvRIeeS.png",
+      "Build/Cruisers/Venture_P/wAOV9Sa.png"
+    ]
+  },
+  "Destroyers": {
+    "Buffalo_MkII": [
+      "Build/Destroyers/Buffalo_MkII/G2kRa5x.png",
+      "Build/Destroyers/Buffalo_MkII/Lx0r3xL.png"
+    ],
+    "Condor": [
+      "Build/Destroyers/Condor/9OEur7a.png",
+      "Build/Destroyers/Condor/LjXAt2S.png",
+      "Build/Destroyers/Condor/ZMA6JVm.png",
+      "Build/Destroyers/Condor/xkOS6Hy.png"
+    ],
+    "Drover": [
+      "Build/Destroyers/Drover/TBkURMS.png"
+    ],
+    "Enforcer": [
+      "Build/Destroyers/Enforcer/XhUqAGP.png",
+      "Build/Destroyers/Enforcer/YPLHCWM.png",
+      "Build/Destroyers/Enforcer/b1MFOxo.png",
+      "Build/Destroyers/Enforcer/mEdupnT.png"
+    ],
+    "Gemini": [
+      "Build/Destroyers/Gemini/ajZk4ZN.png"
+    ],
+    "Hammerhead": [
+      "Build/Destroyers/Hammerhead/07ZWKRf.png",
+      "Build/Destroyers/Hammerhead/1DubIhT.png",
+      "Build/Destroyers/Hammerhead/1EIpy1b.png",
+      "Build/Destroyers/Hammerhead/4RRWvZF.png",
+      "Build/Destroyers/Hammerhead/eKwQRvX.png"
+    ],
+    "Manticore": [
+      "Build/Destroyers/Manticore/tV07bHN.png",
+      "Build/Destroyers/Manticore/xRIkXcf.png"
+    ],
+    "Manticore_LP": [
+      "Build/Destroyers/Manticore_LP/InfwhFJ.png"
+    ],
+    "Medusa": [
+      "Build/Destroyers/Medusa/3CPaVsH.png",
+      "Build/Destroyers/Medusa/45mGCEm.png",
+      "Build/Destroyers/Medusa/olDB0pJ.png"
+    ],
+    "Mule": [
+      "Build/Destroyers/Mule/eco1N8g.png",
+      "Build/Destroyers/Mule/qOSJLyv.png"
+    ],
+    "Shrike": [
+      "Build/Destroyers/Shrike/XxDus95.png",
+      "Build/Destroyers/Shrike/YoJFfyT.png"
+    ],
+    "Sunder": [
+      "Build/Destroyers/Sunder/0yF5Pl4.png",
+      "Build/Destroyers/Sunder/1WFjY9f.png",
+      "Build/Destroyers/Sunder/8dDasZR.png"
+    ]
+  },
+  "Frigates": {
+    "Afflictor": [
+      "Build/Frigates/Afflictor/9YYJLEX.png",
+      "Build/Frigates/Afflictor/yRAHhGt.png"
+    ],
+    "Afflictor_P": [
+      "Build/Frigates/Afflictor_P/yQDt2hK.png"
+    ],
+    "Brawler": [
+      "Build/Frigates/Brawler/94GO4A6.png",
+      "Build/Frigates/Brawler/I2cwp5L.png"
+    ],
+    "Brawler_LP": [
+      "Build/Frigates/Brawler_LP/VtoZ0Kn.png"
+    ],
+    "Brawler_TT": [
+      "Build/Frigates/Brawler_TT/b1u7TaZ.png"
+    ],
+    "Centurion": [
+      "Build/Frigates/Centurion/HPaPCGO.png"
+    ],
+    "Hound": [
+      "Build/Frigates/Hound/hGW4hR2.png"
+    ],
+    "Hound_LP": [
+      "Build/Frigates/Hound_LP/QvNiYbv.png"
+    ],
+    "Hyperion": [
+      "Build/Frigates/Hyperion/QfcDCrG.png",
+      "Build/Frigates/Hyperion/feZ4ESK.png",
+      "Build/Frigates/Hyperion/l8qmDR8.png"
+    ],
+    "Kite_LP": [
+      "Build/Frigates/Kite_LP/8XWu7cu.png"
+    ],
+    "Kite_S": [
+      "Build/Frigates/Kite_S/HYfPGnw.png"
+    ],
+    "Monitor": [
+      "Build/Frigates/Monitor/064vmkK.png",
+      "Build/Frigates/Monitor/OWXP71U.png",
+      "Build/Frigates/Monitor/XHQPx7d.png"
+    ],
+    "Omen": [
+      "Build/Frigates/Omen/wlWTIZS.png"
+    ],
+    "Scarab": [
+      "Build/Frigates/Scarab/4CfzWzM.png",
+      "Build/Frigates/Scarab/z9d7tYB.png"
+    ],
+    "Shade": [
+      "Build/Frigates/Shade/q4KwIfk.png",
+      "Build/Frigates/Shade/vZUVgTS.png"
+    ],
+    "Shade_P": [
+      "Build/Frigates/Shade_P/9Zypmt7.png",
+      "Build/Frigates/Shade_P/hS6Qwra.png"
+    ],
+    "Tempest": [
+      "Build/Frigates/Tempest/275ZuGX.png",
+      "Build/Frigates/Tempest/4LJ8oAr.png"
+    ],
+    "Vanguard": [
+      "Build/Frigates/Vanguard/7QipKSF.png",
+      "Build/Frigates/Vanguard/DPVtKlT.png",
+      "Build/Frigates/Vanguard/NtqNsTZ.png"
+    ],
+    "Vigilance": [
+      "Build/Frigates/Vigilance/9oqUqY3.png",
+      "Build/Frigates/Vigilance/PbUBK7v.png"
+    ],
+    "Wolf": [
+      "Build/Frigates/Wolf/DAx3mAb.png"
+    ]
+  },
+  "SPOILERS": {
+    "Brilliant": [
+      "Build/SPOILERS/Brilliant/hYeSepZ.png"
+    ],
+    "Glimmer": [
+      "Build/SPOILERS/Glimmer/69ZJRKr.png",
+      "Build/SPOILERS/Glimmer/iTaoZLu.png"
+    ],
+    "Radiant": [
+      "Build/SPOILERS/Radiant/POpaQpC.png"
+    ],
+    "Rampart": [
+      "Build/SPOILERS/Rampart/CiHrjFf.png"
+    ],
+    "Scintilla": [
+      "Build/SPOILERS/Scintilla/P9hBCIR.png"
+    ],
+    "Sentry": [
+      "Build/SPOILERS/Sentry/p9aFWFZ.png"
+    ],
+    "Ziggurat": [
+      "Build/SPOILERS/Ziggurat/7oMJgJV.png",
+      "Build/SPOILERS/Ziggurat/mUYCGxV.png",
+      "Build/SPOILERS/Ziggurat/tMU0uQH.png"
+    ]
+  },
+  "SUPER_ALABASTER_REDACTED": {
+    "30020010204015200__1075_range_zapper_Fulgent": [
+      "Build/SUPER_ALABASTER_REDACTED/30020010204015200__1075_range_zapper_Fulgent/PAkSoDW.png",
+      "Build/SUPER_ALABASTER_REDACTED/30020010204015200__1075_range_zapper_Fulgent/kYgPmo4.png",
+      "Build/SUPER_ALABASTER_REDACTED/30020010204015200__1075_range_zapper_Fulgent/oVchOxI.png"
+    ],
+    "50020010204015200__1495_range_ion_cannon_Fulgent": [
+      "Build/SUPER_ALABASTER_REDACTED/50020010204015200__1495_range_ion_cannon_Fulgent/aAamcDJ.png"
+    ],
+    "Astral_Destroyer_of_Worlds": [
+      "Build/SUPER_ALABASTER_REDACTED/Astral_Destroyer_of_Worlds/F4Z1NJh.png"
+    ],
+    "Tesseract": [
+      "Build/SUPER_ALABASTER_REDACTED/Tesseract/DkypdkE.png"
+    ],
+    "Valkyrie-class_Based_Trop_Transport": [
+      "Build/SUPER_ALABASTER_REDACTED/Valkyrie-class_Based_Trop_Transport/6OH2FSf.png"
+    ]
+  },
+  "[REDACTED]": {
+    "Brilliant": [
+      "Build/[REDACTED]/Brilliant/35C5pca.png"
+    ],
+    "Glimmer": [
+      "Build/[REDACTED]/Glimmer/F4AJ13b.png",
+      "Build/[REDACTED]/Glimmer/KLV4mbH.png",
+      "Build/[REDACTED]/Glimmer/XFQWrH7.png"
+    ],
+    "Radiant": [
+      "Build/[REDACTED]/Radiant/COFt3FR.png"
+    ]
+  }
+};
